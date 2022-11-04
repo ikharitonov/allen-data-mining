444a4755874b3a50f98007ad7150706498fe94cf
ikharitonov <sarfins2101@gmail.com>
2022-11-04 15:46:21 +0100

    slice viewer and correlation added

---
+++ proj_slice_viewer.py
@@ -0,0 +1,41 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from allensdk.core.mouse_connectivity_cache import MouseConnectivityCache
+
+class IndexTracker(object):
+    def __init__(self, ax, X):
+        self.ax = ax
+        ax.set_title('use scroll wheel to navigate images')
+
+        self.X = X
+        rows, cols, self.slices = X.shape
+        self.ind = self.slices//2
+
+        self.im = ax.imshow(self.X[:, :, self.ind], cmap='hot')
+        self.update()
+
+    def onscroll(self, event):
+        print("%s %s" % (event.button, event.step))
+        if event.button == 'up':
+            self.ind = (self.ind + 1) % self.slices
+        else:
+            self.ind = (self.ind - 1) % self.slices
+        self.update()
+
+    def update(self):
+        self.im.set_data(self.X[:, :, self.ind])
+        ax.set_ylabel('slice %s' % self.ind)
+        self.im.axes.figure.canvas.draw()
+
+mcc = MouseConnectivityCache()
+
+experiment_id = 294481346
+
+pd, pd_info = mcc.get_projection_density(experiment_id)
+
+fig, ax = plt.subplots(1, 1)
+
+tracker = IndexTracker(ax, np.transpose(pd, (1, 2, 0)))
+
+fig.canvas.mpl_connect('scroll_event', tracker.onscroll)
+plt.show()
