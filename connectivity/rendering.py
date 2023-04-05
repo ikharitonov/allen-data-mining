457fe7d3ceb859f01150fd1c3d803e0763cdbe69
ikharitonov <sarfins2101@gmail.com>
2023-04-05 11:54:05 +0200

    captions with area acronyms

--- connectivity/rendering.py
+++ connectivity/rendering.py
@@ -8,22 +8,34 @@ from pathlib import Path
 # Atlas dimensions
 # 13200 µm x 8000 µm x 11400 µm
 
+# Parameters
+area = 'VISam'
+projection_metric = 'normalized_projection_volume'
+hemisphere_id = 1
+projection_type = 'ipsilateral'
+injection_volume_threshold = 0.5
+
 # Reading the file with centroids data
 path = Path.home() / 'Desktop' / 'data' / 'connectivity'
-filename = path / 'ipsilateral_centroids_dict_hem_1_VISam.pkl'
-with open(filename, 'rb') as f: ipsilateral_centroids_dict = pickle.load(f)
+foldername = f'centroids_{projection_metric}_hem_id_{hemisphere_id}_inj_vol_thresh_{injection_volume_threshold}_{area}'
+filename = f'{projection_type}_centroids_dict_hem_{hemisphere_id}_inj_vol_thresh_{injection_volume_threshold}_{area}.pkl'
+load_path = path / foldername / filename
+with open(load_path, 'rb') as f: centroids_dict = pickle.load(f)
 
 # Extracting coordinates and values of projection metric
-vals = ipsilateral_centroids_dict.values()
-area_ids = list(ipsilateral_centroids_dict.keys())
+vals = centroids_dict.values()
+area_ids = list(centroids_dict.keys())
 xyz = [area[:3] for area in vals]
 proj_metric = [area[3] for area in vals]
+
+# Reading acronyms corresponding to area ids
+with open(path / 'areas_acronyms.pkl', 'rb') as f: areas_acronyms_dict = pickle.load(f)
 
 # Defining data normalisation
 norm = matplotlib.colors.Normalize(vmin=min(proj_metric), vmax=max(proj_metric))
 
 # Create a brainrender scene
-scene = Scene(title=filename.name)
+scene = Scene(title=f'{projection_type} {area} hem={hemisphere_id} inj_vol_thresh={injection_volume_threshold} {projection_metric}')
 
 # Adding brain regions to the plot
 scene.add_brain_region("VISam", "VISpm", "RSPagl", alpha=0.2, color="green")
@@ -44,7 +56,7 @@ for i in range(len(xyz)):
     scene.add(Point(xyz[i], name="invisible areas", alpha=0,radius=1))
     # Plotting the caption
     actors = scene.get_actors()
-    actors[-1].caption(str(area_ids[i]),size=(0.05, 0.01))
+    actors[-1].caption(str(areas_acronyms_dict[area_ids[i]]),size=(0.05, 0.01))
 # Plot all points as a 'Points' object
 scene.add(Points(np.array(xyz),name='Areas',colors=hex_colors,radius=75))
 
